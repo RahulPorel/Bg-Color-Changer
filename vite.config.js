288c02fe33d17b13ee1544389c7b18ffa10f24a4
rahulporel51 <rahulporel51@gmail.com>
2024-02-28 16:25:35 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Bg-Color-Changer",
+  base: "/Bg-Color-Changer/",
   plugins: [react()],
 });
