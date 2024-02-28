2e501a9ed5ebe82e7542430640819febaf01a7b0
rahulporel51 <rahulporel51@gmail.com>
2024-02-28 16:21:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import RenderMsgOnLoad from "./components/RenderMsgOnLoad";
+
+function App() {
+  const [bgColor, setBgColor] = useState("navy");
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: bgColor, fontWeight: "bolder" }}
+    >
+     
+
+      <h1
+        className="flex justify-center "
+        style={{
+          color: "rgb(30, 255, 0)",
+          paddingTop: "17.5rem",
+          fontSize: "50px",
+        }}
+      >
+         <RenderMsgOnLoad />
+        Color: {bgColor}{" "}
+      </h1>
+
+      
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-2 rounded-xl">
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+            onClick={() => {
+              setBgColor("red");
+            }}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+            onClick={() => {
+              setBgColor("green");
+            }}
+          >
+            Green
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => {
+              setBgColor("blue");
+            }}
+          >
+            Blue
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "black" }}
+            onClick={() => {
+              setBgColor("black");
+            }}
+          >
+            Black
+          </button>
+
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "grey" }}
+            onClick={() => {
+              setBgColor("grey");
+            }}
+          >
+            Grey
+          </button>
+
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "purple" }}
+            onClick={() => {
+              setBgColor("purple");
+            }}
+          >
+            Purple
+          </button>
+
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg "
+            style={{ backgroundColor: "yellow", color: "black" }}
+            onClick={() => {
+              setBgColor("yellow");
+            }}
+          >
+            Yellow
+          </button>
+
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "white", color: "black" }}
+            onClick={() => {
+              setBgColor("white");
+            }}
+          >
+            White
+          </button>
+
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "olive" }}
+            onClick={() => {
+              setBgColor("olive");
+            }}
+          >
+            olive
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RenderMsgOnLoad.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+export default function RenderMsgOnLoad() {
+  return useEffect(() => {
+    <h1>dfssdfgdsf</h1>
+    // function render() {
+    //   <h1>Click Below Buttons to Change Background Color of the Page</h1>;
+    // }
+  }, []);
+}
